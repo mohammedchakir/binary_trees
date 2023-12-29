2fbe8fb1bc60325d4527dc1e682266184fcd31df
mohammedchakir <zerroukimedchakir@gmail.com>
2023-12-29 17:10:39 +0100

    adding

--- binary_trees.h
+++ binary_trees.h
@@ -13,13 +13,14 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-    int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+	int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 };
+
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
@@ -28,7 +29,7 @@ typedef struct binary_tree_s heap_t;
 /* binary_tree_print.c */
 void binary_tree_print(const binary_tree_t *);
 
-/* binary_trees functions */
+/* Prototypes of functions */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
