240393271226be10dff55044402a116eceb97da2
mohammedchakir <zerroukimedchakir@gmail.com>
2023-12-29 15:48:54 +0100

    adding files

--- binary_trees.h
+++ binary_trees.h
@@ -20,6 +20,7 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
