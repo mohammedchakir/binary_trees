0c21e73dc6bf14d041b31daf675210a912355eca
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-02 03:38:33 -0800

    updating betty style

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -4,27 +4,28 @@
  * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
  * @first: Pointer to the first node
  * @second: Pointer to the second node
- * 
+ *
  * Return: Pointer to the lowest common ancestor node of the two nodes
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
-    const binary_tree_t *tmp;
+	const binary_tree_t *tmp;
 
-    if (!first || !second)
-        return (NULL);
+	if (!first || !second)
+		return (NULL);
 
-    while (first)
-    {
-        tmp = second;
-        while (tmp)
-        {
-            if (first == tmp)
-                return ((binary_tree_t *)first);
-            tmp = tmp->parent;
-        }
-        first = first->parent;
-    }
+	while (first)
+	{
+		tmp = second;
+		while (tmp)
+		{
+			if (first == tmp)
+				return ((binary_tree_t *)first);
+			tmp = tmp->parent;
+		}
+		first = first->parent;
+	}
 
-    return (NULL);
+	return (NULL);
 }
--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,33 +1,35 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_levelorder - goes through a binary tree using level-order traversal
+ * binary_tree_levelorder - goes through a binary tree using
+ * level-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-    binary_tree_t **queue;
-    int front = 0, rear = -1, size = 1024;
+	binary_tree_t **queue;
+	int front = 0, rear = -1, size = 1024;
 
-    if (!tree || !func)
-        return;
+	if (!tree || !func)
+		return;
 
-    queue = malloc(sizeof(binary_tree_t *) * size);
-    if (!queue)
-        return;
+	queue = malloc(sizeof(binary_tree_t *) * size);
+	if (!queue)
+		return;
 
-    queue[++rear] = (binary_tree_t *)tree;
+	queue[++rear] = (binary_tree_t *)tree;
 
-    while (front <= rear)
-    {
-        binary_tree_t *node = queue[front++];
-        func(node->n);
-        if (node->left)
-            queue[++rear] = node->left;
-        if (node->right)
-            queue[++rear] = node->right;
-    }
+	while (front <= rear)
+	{
+		binary_tree_t *node = queue[front++];
 
-    free(queue);
+		func(node->n);
+		if (node->left)
+			queue[++rear] = node->left;
+		if (node->right)
+			queue[++rear] = node->right;
+	}
+
+	free(queue);
 }
