f8ccaadb200ee321de1c86b62f8a67d7d716d409
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-03 18:53:48 +0100

    add

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -4,22 +4,24 @@
 /**
  * Checks if a binary tree is a BST.
  */
-int is_bst(const binary_tree_t *tree, int min, int max) {
+int is_bst(const binary_tree_t *tree, int min, int max)
+{
     if (tree == NULL)
-        return 1;
+        return (1);
     if (tree->n <= min || tree->n >= max)
-        return 0;
-    return is_bst(tree->left, min, tree->n) && is_bst(tree->right, tree->n, max);
+        return (0);
+    return (is_bst(tree->left, min, tree->n) && is_bst(tree->right, tree->n, max));
 }
 
 /**
  * Calculates the height of a binary tree and checks if it's balanced.
  */
-int check_height_balance(const binary_tree_t *tree, int *is_balanced) {
+int check_height_balance(const binary_tree_t *tree, int *is_balanced)
+{
     int left_height, right_height;
 
     if (tree == NULL)
-        return 0;
+        return (0);
 
     left_height = check_height_balance(tree->left, is_balanced);
     right_height = check_height_balance(tree->right, is_balanced);
@@ -27,21 +29,22 @@ int check_height_balance(const binary_tree_t *tree, int *is_balanced) {
     if (abs(left_height - right_height) > 1)
         *is_balanced = 0;
 
-    return (left_height > right_height ? left_height : right_height) + 1;
+    return ((left_height > right_height ? left_height : right_height) + 1);
 }
 
 /**
  * Checks if a binary tree is a valid AVL Tree.
  */
-int binary_tree_is_avl(const binary_tree_t *tree) {
-    int is_balanced = 1;
-
-    if (tree == NULL)
-        return 0;
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	int is_balanced = 1;
+	
+	if (tree == NULL)
+	return (0);
 
     if (!is_bst(tree, INT_MIN, INT_MAX))
-        return 0;
+        return (0);
 
     check_height_balance(tree, &is_balanced);
-    return is_balanced;
+    return (is_balanced);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -75,4 +75,8 @@ int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
 
+/* Prototypes helper functions */
+int is_bst(const binary_tree_t *tree, int min, int max);
+int check_height_balance(const binary_tree_t *tree, int *is_balanced);
+
 #endif /* BINARY_TREES_H */
