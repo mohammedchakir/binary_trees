a025d8c9df1fae37fb1f956cb1f0b81e229020b8
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-03 02:21:58 +0100

    forgot to pull before pushing

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,20 +4,18 @@
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure the height
  *
- * Return: Height of the binary tree, 0 if tree is NULL
+ * Return: The height of the tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-    if (tree == NULL)
-        return (0);
+	size_t left_height = tree->left ? binary_tree_height(tree->left) : 0;
+	size_t right_height = tree->right ? binary_tree_height(tree->right) : 0;
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-
-    if (left_height > right_height)
-        return (left_height + 1);
-    else
-        return (right_height + 1);
+	return ((left_height > right_height ? left_height : right_height)
+			+ (tree->left || tree->right));
 }
