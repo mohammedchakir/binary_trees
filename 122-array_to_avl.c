1a35522a801e2d8c7dff7334b29a2a965ea20347
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-04 01:30:55 +0100

    testing

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -5,22 +5,16 @@
  * @array: Pointer to the first element of the array
  * @size: Number of elements in the array
  *
- * Return: Pointer to the root node of the created AVL tree, or NULL on failure
+ * Return: Pointer to the root node of the created AVL tree,
+ * or NULL on failure
  */
 avl_t *array_to_avl(int *array, size_t size)
 {
-    if (!array || size == 0)
-        return NULL;
-
+    unsigned int i;
     avl_t *root = NULL;
-    avl_t *node = NULL;
+    
+    for (i = 0; i < size; i++)
+    avl_insert(&root, array[i]);
 
-    for (size_t i = 0; i < size; ++i)
-    {
-        node = avl_insert(&root, array[i]);
-        if (!node) {
-        }
-    }
-
-    return root;
+	return (root);
 }
