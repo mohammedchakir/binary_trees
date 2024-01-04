a7ddfb9c379fe8b3635e901534ead712def91e25
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-04 03:38:53 +0100

    test

--- binary_trees.h
+++ binary_trees.h
@@ -78,7 +78,7 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size);
 /* Prototypes helper functions */
 int is_bst(const binary_tree_t *tree, int min, int max);
 int check_height_balance(const binary_tree_t *tree, int *is_balanced);
-avl_t *sorted_array_to_avl_helper(int *array, size_t lo, size_t hi, avl_t *parent);
+
 
 
 #endif /* BINARY_TREES_H */
